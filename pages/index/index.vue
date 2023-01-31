bb3cdd97ef67ef4beb8014d4eb3e420ec079b969
bais <1121827639@qq.com>
2023-01-31 13:57:49 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="container">
+		
+		<view class="intro">这是首页</view>
+		<button type="primary" @click="Go" >跳转</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			Go(){
+				uni.navigateTo({
+					url: "/pages/sub/index"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+	.intro{
+		text-align: center;
+		margin-bottom: 15rpx;
+	}
+</style>
